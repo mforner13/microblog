9d56e47318d918eb557d906a4c3c6cff72613fdc
Miriam Forner <mforner13@gmail.com>
2019-06-06 23:00:35 +0100

    Show other users' blog entries & initial commit for base template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = { 'username': 'Miguel' }
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Leeds!'
+        },
+        {
+            'author': {'username': 'Sally'},
+            'body': 'Avengers end game was pretty good!'
+        },
+        {
+            'author': {'username': 'Eddy'},
+            'body': 'Game of thrones? More like game of unused characterisation!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
