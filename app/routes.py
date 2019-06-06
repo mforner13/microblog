e55152b02f869770e90a2fce0f2b323ee7a88264
Miriam Forner <mforner13@gmail.com>
2019-06-06 21:43:11 +0100

    Initial commit for template file & render template

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-          return "Hello world"
+    user = { 'username': 'Miguel' }
+    return render_template('index.html', title='Home', user=user)
