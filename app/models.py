79f263d174e0d90184cff20cbb6c719c224b8c1a
Miriam forner <miriamforner@Mo-Macbook-Pro-with-the-touchbar-and-bluetooth-mouse.local>
2019-07-31 21:01:33 +0100

    Compare users against user table, log in and log out view functions.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
+from flask_login import UserMixin
+from app import login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -12,6 +15,16 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,7 +41,7 @@ class Masterclass(db.Model):
     description = db.Column(db.String(400))
     timestamp = db.Column(db.DateTime, index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    location = db.relationship('Location', backref='location', lazy='dynamic')
+    location = db.relationship('Location', backref='masterclass', lazy='dynamic')
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
